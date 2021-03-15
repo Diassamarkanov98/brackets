dc1be5f9e68338264d70d56c36136974a603b4ee
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-03-15 18:53:01 +0600

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let stackArray = [];
+
+    for( let i = 0; i < str.length; i++ ){
+        for ( let bracket of bracketsConfig ) {
+            if( str[i] == bracket[0]  && bracket[0] == bracket[1]
+                && bracket[0] == stackArray[stackArray.length - 1] ){
+                stackArray.pop();
+                break;
+            }
+            if ( str[i] == bracket[0] ){
+                stackArray.push( str[i] );
+                break;
+            }
+            if( str[i] == bracket[1] ){
+                if( bracket[0] != stackArray.pop() ){
+                    return false;
+                }
+            }
+        }
+    }
+    if( stackArray.length ) return false;
+    return true;
 }
